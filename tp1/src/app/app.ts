0c507cf283383e44abfa6e563b2169208ab4458c
myr4j <myrtgnt@gmail.com>
2025-09-09 16:58:55 +0200

    dev purple header with names in white

--- tp1/src/app/app.ts
+++ tp1/src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Header} from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('tp1');
+
+  public textColorAlign : string = "names";
+  public headerSize : string = "size";
+  public headerBackground : string = "background";
 }
---
+++ tp1/src/app/header/header.ts
@@ -0,0 +1,17 @@
+import {Component, Input} from '@angular/core';
+import {NgClass} from '@angular/common';
+
+@Component({
+  selector: 'Header',
+  imports: [
+    NgClass
+  ],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header {
+
+  @Input() names: string = '';
+  @Input() size: string = '';
+  @Input() background: string = '';
+}
